75977edba54c44d5a56a030da09be8df4f4964cf
Vasiliy Vanchuk (@vvscode) <v.vanchuk@tut.by>
2019-10-09 16:11:18 +0400

    Add --ws option

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ if (!target.startsWith("http")) {
   process.exit(console.log("Pass url as last argument"));
 }
 
+let tunnel = null;
+if (process.argv.includes('--tunnel')) {
+  tunnel = true;
+}
+let useWs = undefined;
+if (process.argv.includes('--ws')) {
+  useWs = true;
+}
+
 const port = process.env.BROWSER_SYNC_PORT || 10000;
 const uiPort = process.env.BROWSER_SYNC_UI_PORT || 10001;
 
@@ -22,16 +31,22 @@ bs.init({
 
         const newCsp = new CspParse(cspHeader);
 
+        if (useWs) {
+          newCsp.add("connect-src", `http://*:${uiPort}`);
+          newCsp.add("connect-src", `https://*:${uiPort}`);
+        }
         newCsp.add("script-src", "'unsafe-inline'");
         newCsp.add("script-src", "'sha256-ThhI8UaSFEbbl6cISiZpnJ4Z44uNSq2tPKgyRTD3LyU='");
         const newCspString = newCsp.toString();
 
         proxyRes.headers["content-security-policy"] = newCspString;
       }
-    ]
+    ],
+    ws: useWs,
   },
   port,
   ui: {
     port: uiPort,
-  }
+  },
+  tunnel,
 });
